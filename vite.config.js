2e7350739257c0346fcfbac7414a6364c4f7c269
vkivela <kivelaville@hotmail.com>
2026-01-17 22:39:07 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bookstorereact/', // add this property and the value is the name of your repository
+  base: '/bookstore/', // add this property and the value is the name of your repository
   plugins: [react()],
   test: {
     globals: true,
