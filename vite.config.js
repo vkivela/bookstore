fcfca5d1d26c2d78790516cbf0f234d3d5c086c0
vkivela <kivelaville@hotmail.com>
2026-01-17 22:10:24 +0200

    deploy app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/bookstorereact/', // add this property and the value is the name of your repository
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+  build: {
+    outDir: 'build', // change default output folder
+  }, 
 })
